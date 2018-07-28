7d5890583a4b3b4b27125653483c6354efa5bad4
Chamoniix <hugo4242@hotmail.fr>
2018-07-28 17:55:19 +0200

    photo et objet post

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Post } from './post.class';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'portofolio';
+
+  title = 'Gybels Photo';
+  private post = new Post();
+
+  constructor() {
+    this.post.id = 1;
+    this.post.description = 'Alios autem dicere aiunt multo etiam inhumanius (quem locum breviter paulo ante perstrinxi) praesidii adiumentique causa, non benevolentiae neque caritatis, amicitias esse expetendas; itaque, ut quisque minimum firmitatis haberet minimumque virium, ita amicitias appetere maxime; ex eo fieri ut mulierculae magis amicitiarum praesidia quaerant quam viri et inopes quam opulenti et calamitosi quam ii qui putentur beati.';
+    this.post.titre = 'Le chaton';
+    this.post.url = 'src/ressources/img/MAR_5586.JPG';
+    this.post.date = new Date();
+  }
+
+
 }
